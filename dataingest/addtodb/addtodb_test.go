dfd9a98e083c9d80b9a841ecf9c7ec32175b5bb4
Vivian Allen <vivian.allen@methods.co.uk>
2022-05-03 20:26:01 +0100

    add output areas to geotypes ingested by dataingest
    Remove regexp switch ignoring output area files when ingesting
    NOMIS data.
    Refactor geotype detection in data ingest to use GSS codes in data
    rather than filenames, as the paradigm used to ID them breaks down
    a bit with output areas.
    Refactor tests to assert geotypes are correctly identified and
    data from output area NOMIS files are ingestable.

--- dataingest/addtodb/addtodb_test.go
+++ dataingest/addtodb/addtodb_test.go
@@ -46,7 +46,13 @@ func TestGetFiles(t *testing.T) {
 
 	di.getFiles("testdata/")
 
-	if di.files.data[0] != "testdata/QS104EWDATA04.CSV" || di.files.meta[0] != "testdata/QS104EWMETA0.CSV" || di.files.desc[0] != "testdata/QS104EWDESC0.CSV" {
+	for _, fn := range di.files.data {
+		if fn != "testdata/QS104EWDATA01_A.CSV" && fn != "testdata/QS104EWDATA04.CSV" {
+			t.Fail()
+		}
+	}
+
+	if di.files.meta[0] != "testdata/QS104EWMETA0.CSV" || di.files.desc[0] != "testdata/QS104EWDESC0.CSV" {
 		t.Fail()
 	}
 }
@@ -130,19 +136,59 @@ func TestAddGeoGeoMetricData(t *testing.T) {
 		di.pool = pool
 
 		pool.Exec(ctx, "INSERT INTO geo_type VALUES(4,'LAD')")
+		pool.Exec(ctx, "INSERT INTO geo_type VALUES(7,'OA')")
 		pool.Exec(ctx, "INSERT INTO NOMIS_DESC (id,name,pop_stat,short_nomis_code,year,nomis_topic_id) VALUES (66,'Sex','All usual residents','QS104EW',2011,1)")
 		pool.Exec(ctx, "INSERT INTO NOMIS_CATEGORY (id,nomis_desc_id,category_name,measurement_unit,stat_unit,long_nomis_code,year) VALUES (3,66,'All categories: Sex','Count','Person','QS104EW0001',2011)")
 		pool.Exec(ctx, "INSERT INTO NOMIS_CATEGORY (id,nomis_desc_id,category_name,measurement_unit,stat_unit,long_nomis_code,year) VALUES (4,66,'All categories: Sex','Count','Person','QS104EW0002',2011)")
 
-		di.files.data = []string{"testdata/QS104EWDATA04.CSV"}
+		di.files.data = []string{"testdata/QS104EWDATA04.CSV", "testdata/QS104EWDATA01_A.CSV"}
 		di.addGeoGeoMetricData(map[string]int32{"QS104EW0001": 3, "QS104EW0002": 4})
 
-		var metric float64
-		if err := pool.QueryRow(ctx, "SELECT metric FROM geo_metric WHERE category_id=3").Scan(&metric); err != nil {
-			log.Print(err)
+		var returned float64
+		var expected float64
+
+		// check LAD result for QS104EW0001 matches
+		if err := pool.QueryRow(
+			ctx,
+			`
+SELECT
+	geo_metric.metric
+FROM
+	geo_metric,
+	geo
+WHERE geo_metric.geo_id = geo.id
+AND geo.type_id = 4
+AND geo_metric.category_id = 3
+`,
+		).Scan(&returned); err != nil {
+			t.Error(err)
 		}
 
-		if metric != 92028 {
+		expected = 92028
+		if returned != expected {
+			t.Logf(fmt.Sprintf("Expected %v value for test LAD, got %v", expected, returned))
+			t.Fail()
+		}
+
+		// check OA result for QS104EW0002 matches
+		if err := pool.QueryRow(
+			ctx,
+			`
+SELECT
+	geo_metric.metric 
+FROM
+	geo_metric,
+	geo
+WHERE geo_metric.geo_id = geo.id
+AND geo.type_id = 7
+AND geo_metric.category_id = 4
+`,
+		).Scan(&returned); err != nil {
+			t.Error(err)
+		}
+		expected = 54751
+		if returned != expected {
+			t.Logf(fmt.Sprintf("Expected %v value for test OA, got %v", expected, returned))
 			t.Fail()
 		}
 
@@ -155,4 +201,42 @@ func TestAddGeoGeoMetricData(t *testing.T) {
 
 	}()
 
+}
+
+func TestGeotypeIDFromGSSCode(t *testing.T) {
+	di := New("2011", dsn)
+	testGSSCodes := map[string]int{
+		// EW test
+		"K04000001": 1,
+		// Country tests
+		"E92000001": 2,
+		"W92000004": 2,
+		// region tests
+		"E12000004": 3,
+		// LAD tests,
+		"E06000027": 4,
+		"W06000023": 4,
+		"E07000009": 4,
+		"E08000007": 4,
+		"E09000003": 4,
+		// MSOA tests
+		"E02000030": 5,
+		"W02000405": 5,
+		// LSOA tests
+		"E01000019": 6,
+		"W01001952": 6,
+		// OA tests
+		"E00080972": 7,
+		"W00000025": 7,
+	}
+	for gssCode, expected := range testGSSCodes {
+		returned, err := di.geotypeIDFromGSSCode(gssCode)
+		if err != nil {
+			t.Error(err)
+		}
+		if returned != expected {
+			t.Logf(fmt.Sprintf("Expected geotype ID %d for GSS code %s, got %d", expected, gssCode, returned))
+			t.Fail()
+		}
+	}
 }
