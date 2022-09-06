0d5d14321d8109308c3211251cf21acafcf4ce21
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2022-09-06 10:42:22 +0100

    initial map-categories program

---
+++ data-tiles/cmd/map-categories/content/content.go
@@ -0,0 +1,79 @@
+// Load and save content.json
+package content
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Content struct {
+	Meta        Meta         `json:"meta"`
+	TopicGroups []TopicGroup `json:"content"`
+}
+
+type Meta struct {
+	FirstCreatedAt                      string `json:"first_created_at"`
+	CantabularMetadataSource            string `json:"cantabular_metadata_source"`
+	FilteredToAtlasContentAt            string `json:"filtered_to_atlas_content_at"`
+	RichContentSpecFileUsedToFilter     string `json:"rich_content_spec_file_used_to_filter"`
+	LegendStrsUpdatedAt                 string `json:"legend_strs_updated_at"`
+	LegendStrsFileUsed                  string `json:"legend_strs_file_used"`
+	PlaceholderVariablesDescsInsertedAt string `json:"placeholder_variable_descs_inserted_at"`
+}
+
+type TopicGroup struct {
+	Name   string  `json:"name"`
+	Slug   string  `json:"slug"`
+	Desc   string  `json:"desc"`
+	Topics []Topic `json:"topics"`
+}
+
+type Topic struct {
+	Name      string     `json:"name"`
+	Slug      string     `json:"slug"`
+	Desc      string     `json:"desc"`
+	Variables []Variable `json:"variables"`
+}
+
+type Variable struct {
+	Name            string           `json:"name"`
+	Code            string           `json:"code"`
+	Slug            string           `json:"slug"`
+	Desc            string           `json:"desc"`
+	Units           string           `json:"units"`
+	Classifications []Classification `json:"classifications"`
+}
+
+type Classification struct {
+	Code                        string     `json:"code"`
+	Slug                        string     `json:"slug"`
+	Desc                        string     `json:"desc"`
+	ChoroplethDefault           bool       `json:"choropleth_default,omitempty"`
+	DotDensityDefault           bool       `json:"dot_density_default,omitempty"`
+	Comparison2011DataAvailable bool       `json:"comparison_2011_data_available"`
+	Categories                  []Category `json:"categories"`
+}
+
+type Category struct {
+	Name       string `json:"name"`
+	Slug       string `json:"slug"`
+	Code       string `json:"code"`
+	LegendStr1 string `json:"legend_str_1"`
+	LegendStr2 string `json:"legend_str_2"`
+	LegendStr3 string `json:"legend_str_3"`
+}
+
+func Load(r io.Reader) (*Content, error) {
+	dec := json.NewDecoder(r)
+	var c Content
+	if err := dec.Decode(&c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+func (c *Content) Save(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "    ")
+	return enc.Encode(c)
+}
---
+++ data-tiles/cmd/map-categories/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ONSdigital/dp-geodata-api/data-tiles/cmd/map-categories/content"
+)
+
+func main() {
+	cfile := flag.String("c", "content.json", "path to content.json")
+	flag.Parse()
+
+	c, err := loadContent(*cfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	records, err := loadSpreadsheet(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmap := buildMap(c)
+	headings, keepcols := convertHeadings(records[0], cmap)
+
+	cwriter := csv.NewWriter(os.Stdout)
+	if err := cwriter.Write(headings); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, record := range records[1:] {
+		var newrecord []string
+		for _, col := range keepcols {
+			newrecord = append(newrecord, record[col])
+		}
+		if err := cwriter.Write(newrecord); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	cwriter.Flush()
+	if err := cwriter.Error(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// loadContent loads content.json.
+func loadContent(name string) (*content.Content, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return content.Load(f)
+}
+
+// loadSpreadsheet loads the metrics CSV.
+func loadSpreadsheet(f io.Reader) ([][]string, error) {
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	if len(records) == 0 {
+		return nil, errors.New("empty spreadsheet")
+	}
+	return records, nil
+}
+
+// buildMap creates the map from category name to category code.
+func buildMap(c *content.Content) map[string]string {
+	catmap := map[string]string{}
+
+	for _, group := range c.TopicGroups {
+		for _, topic := range group.Topics {
+			for _, variable := range topic.Variables {
+				for _, classification := range variable.Classifications {
+					for _, category := range classification.Categories {
+						catmap[category.Name] = category.Code
+					}
+				}
+			}
+		}
+	}
+	return catmap
+}
+
+// convertHeadings renames headings that have mappings, and records which
+// columns we want to include in the output spreadsheet.
+func convertHeadings(headings []string, cmap map[string]string) ([]string, []int) {
+	var newheadings []string
+	var keepcols []int
+
+	for i, heading := range headings {
+		newheading := lookupHeading(heading, cmap)
+		if newheading == "" {
+			continue
+		}
+		newheadings = append(newheadings, newheading)
+		keepcols = append(keepcols, i)
+	}
+
+	return newheadings, keepcols
+}
+
+// lookupHeading maps a single heading, or returns "" if the heading should not
+// or could not be mapped.
+func lookupHeading(header string, cmap map[string]string) string {
+	if header == "geog_code" {
+		return "GeographyCode"
+	}
+	if header == "geog_label" {
+		return ""
+	}
+
+	// split year and description
+	year, desc, found := strings.Cut(header, " ")
+	if !found {
+		return ""
+	}
+	if year != "2021" {
+		return ""
+	}
+
+	// lookup description in map
+	newheader, found := cmap[desc]
+	if !found {
+		return ""
+	}
+
+	return year + "-" + newheader
+}
