8370ac84397b903b554d9d28baef8c46431d2124
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2022-09-14 11:01:41 +0100

    Initial support for generating fake data: * add met-fake.mk * support params.mk in GNUmakefile * generate categories.txt from content.json

---
+++ data-tiles/met-fake.mk
@@ -0,0 +1,22 @@
+#
+# met-fake.mk -- rules to generate fake metrics
+#
+# This makefile is mean to be included by GNUmakefile; it's not independent
+#
+# Variables this file is expected to set
+#
+#	MET_DOWNLOADS	list of downloaded metrics files
+#	MET_STANDARD	list of normalised standard format metrics files
+#
+# Each file named in those variables should have targets in this file.
+# And there should be clean and realclean targets to clean these files up.
+
+MET_DOWNLOADS=$(DDMV)/DATA.CSV
+
+$(MET_DOWNLOADS): fake-data $(GEO_STANDARD) $(CONTENT)
+	./atomic.sh "$(MET_DOWNLOADS)" ./fake-data -c "$(CONTENT)" "$(GEO_STANDARD)"
+
+realclean::
+	rm -f "$(MET_DOWNLOADS)"
+
+MET_STANDARD=$(MET_DOWNLOADS)
