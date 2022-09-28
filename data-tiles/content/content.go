9b4c46c555e6637edd8941e3282c5675719ac5f7
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2022-09-28 10:38:18 +0100

    support for new content.json format

--- data-tiles/content/content.go
+++ data-tiles/content/content.go
@@ -3,14 +3,27 @@ package content
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 )
 
 type Content struct {
+	old *oldContent
+	new *newContent
+}
+
+// old structure of content.json
+type oldContent struct {
 	Meta        Meta         `json:"meta"`
 	TopicGroups []TopicGroup `json:"content"`
+}
+
+// new structure of content.json
+type newContent struct {
+	Meta   Meta    `json:"meta"`
+	Topics []Topic `json:"content"`
 }
 
 type Meta struct {
@@ -66,12 +79,57 @@ type Category struct {
 }
 
 func Load(r io.Reader) (*Content, error) {
-	dec := json.NewDecoder(r)
-	var c Content
-	if err := dec.Decode(&c); err != nil {
+	buf, err := io.ReadAll(r)
+	if err != nil {
 		return nil, err
 	}
-	return &c, nil
+
+	new, err := newLoad(buf)
+	if err != nil {
+		return nil, err
+	}
+	if new.isValid() {
+		return &Content{
+			new: new,
+		}, nil
+	}
+
+	old, err := oldLoad(buf)
+	if err != nil {
+		return nil, err
+	}
+	if len(old.TopicGroups) > 0 {
+		return &Content{
+			old: old,
+		}, nil
+	}
+
+	return nil, errors.New("could not understand content.json")
+}
+
+// isValid is true if n holds a sensible new contents.json
+func (n *newContent) isValid() bool {
+	numVars := 0
+	for _, topic := range n.Topics {
+		numVars += len(topic.Variables)
+	}
+	return numVars > 0
+}
+
+func oldLoad(buf []byte) (*oldContent, error) {
+	var old oldContent
+	if err := json.Unmarshal(buf, &old); err != nil {
+		return nil, err
+	}
+	return &old, nil
+}
+
+func newLoad(buf []byte) (*newContent, error) {
+	var new newContent
+	if err := json.Unmarshal(buf, &new); err != nil {
+		return nil, err
+	}
+	return &new, nil
 }
 
 func LoadName(name string) (*Content, error) {
@@ -84,15 +142,18 @@ func LoadName(name string) (*Content, error) {
 	return Load(f)
 }
 
-func (c *Content) Save(w io.Writer) error {
-	enc := json.NewEncoder(w)
-	enc.SetIndent("", "    ")
-	return enc.Encode(c)
+func (c *Content) Categories() []string {
+	if c.new != nil {
+		return c.newCategories()
+	} else {
+		return c.oldCategories()
+	}
 }
 
-func (c *Content) Categories() []string {
+func (c *Content) oldCategories() []string {
 	var cats []string
-	for _, group := range c.TopicGroups {
+
+	for _, group := range c.old.TopicGroups {
 		for _, topic := range group.Topics {
 			for _, variable := range topic.Variables {
 				for _, classification := range variable.Classifications {
@@ -106,13 +167,36 @@ func (c *Content) Categories() []string {
 	return cats
 }
 
+func (c *Content) newCategories() []string {
+	var cats []string
+
+	for _, topic := range c.new.Topics {
+		for _, variable := range topic.Variables {
+			for _, classification := range variable.Classifications {
+				for _, cat := range classification.Categories {
+					cats = append(cats, cat.Code)
+				}
+			}
+		}
+	}
+	return cats
+}
+
 // NamesToCats creates a map to lookup a category code given a category name.
 // This is used when converting plain text headings in spreadsheets to specific
 // category codes.
 func (c *Content) NamesToCats(classcode string) (map[string]string, error) {
+	if c.new != nil {
+		return c.newNamesToCats(classcode)
+	} else {
+		return c.oldNamesToCats(classcode)
+	}
+}
+
+func (c *Content) oldNamesToCats(classcode string) (map[string]string, error) {
 	catmap := map[string]string{}
 
-	for _, group := range c.TopicGroups {
+	for _, group := range c.old.TopicGroups {
 		for _, topic := range group.Topics {
 			for _, variable := range topic.Variables {
 				for _, classification := range variable.Classifications {
@@ -129,6 +213,31 @@ func (c *Content) NamesToCats(classcode string) (map[string]string, error) {
 						}
 						catmap[category.Name] = category.Code
 					}
+				}
+			}
+		}
+	}
+	return catmap, nil
+}
+
+func (c *Content) newNamesToCats(classcode string) (map[string]string, error) {
+	catmap := map[string]string{}
+
+	for _, topic := range c.new.Topics {
+		for _, variable := range topic.Variables {
+			for _, classification := range variable.Classifications {
+				if classification.Code != classcode {
+					continue
+				}
+				for _, category := range classification.Categories {
+					_, ok := catmap[category.Name]
+					if ok {
+						return nil, fmt.Errorf(
+							"duplicate: %q",
+							category.Name,
+						)
+					}
+					catmap[category.Name] = category.Code
 				}
 			}
 		}
