ecb417546e444eafac45006ba5f529032972143a
Daniel Lawrence <33934706+wavemechanics@users.noreply.github.com>
2022-09-29 13:09:29 +0100

    update URL for 2021 LAD geojson

--- data-tiles/geo-2021.mk
+++ data-tiles/geo-2021.mk
@@ -14,7 +14,11 @@
 # URLs for raw geojson files
 #
 
-LAD_URL=https://opendata.arcgis.com/api/v3/datasets/d31f826e318d441390f54f472d976ee1_0/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1
+# The LAD geodata is found by:
+# 1. Go to https://geoportal.statistics.gov.uk/datasets/ons::local-authority-districts-december-2021-uk-bgc/explore?location=55.215503%2C-3.316939%2C7.48
+# 2. Select Download
+# 3. Select GeoJSON
+LAD_URL=https://opendata.arcgis.com/api/v3/datasets/0f131e03df73415b824a1c214594eeab_0/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1
 
 MSOA_URL=https://opendata.arcgis.com/api/v3/datasets/38255434eb54456cbd202f54fddfe5c9_0/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1
 
